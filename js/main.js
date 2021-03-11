ee30396b99a849475e6e2141fb9c7a0636099ba2
Apolline Vial <apovial@gmail.com>
2021-03-11 22:26:01 +0100

    marqueurs carte

--- js/main.js
+++ js/main.js
@@ -1,14 +1,88 @@
-$(document).ready(function () {
-    let map;
+let map, infoWindow;
 
-    function initMap() {
-        map = new google.maps.Map(document.getElementById("map"), {
-            center: {
-                lat: -34.397,
-                lng: 150.644
-            },
-            zoom: 8,
-        });
-    }
+function initMap() {
 
-});
+    const bronco = {
+        lat: 48.8737815,
+        lng: 2.3501649
+    };
+    const babalou = {
+        lat: 48.8865035,
+        lng: 2.3442197
+    };
+
+    let map = new google.maps.Map(document.getElementById("map"), {
+        center: bronco,
+        zoom: 10,
+    });
+
+    const marker1 = new google.maps.Marker({
+        position: bronco,
+        map: map,
+    });
+
+    const marker2 = new google.maps.Marker({
+        position: babalou,
+        map: map,
+    });
+
+    infoWindow = new google.maps.InfoWindow();
+
+    const locationButton = document.createElement("button");
+
+    locationButton.textContent = "Pan to Current Location";
+
+    locationButton.classList.add("custom-map-control-button");
+
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+
+    locationButton.addEventListener("click", () => {
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude,
+                    };
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent("Location found.");
+                    infoWindow.open(map);
+                    map.setCenter(pos);
+                },
+                () => {
+                    handleLocationError(true, infoWindow, map.getCenter());
+                }
+            );
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+    });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+        browserHasGeolocation ?
+        "Error: The Geolocation service failed." :
+        "Error: Your browser doesn't support geolocation."
+    );
+
+    infoWindow.open(map);
+}
+
+
+/*Create restaurants*/
+let bronco = new Restaurant("Bronco", "39 Rue des Petites Écuries, 75010 Paris");
+bronco.location.latitude = 48.8737815;
+bronco.location.longitude = 2.3501649;
+
+let babalou = new Restaurant("Babalou", "4 Rue Lamarck, 75018 Paris");
+babalou.location.latitude = 48.8865035;
+babalou.location.longitude = 2.3442197;
+
+/*Créer un tableau des pistolets*/
+var tabRestaurants = [bronco, babalou];
+
+let mymap = new Mymap();
--- js/map.js
+++ js/map.js
@@ -1,8 +1,9 @@
-class Map {
+class Mymap {
     constructor() {
-        this.createMap();
-        this.showRestaurant();
-        this.showUser();
+        /*this.createMap();
+        this.showUser();*/
+        this.showRestaurant(tabRestaurants);
+        
     }
 
 
@@ -10,7 +11,16 @@ class Map {
 
     }
     
-    showRestaurant() {
+    showRestaurant(tabRestaurants) {
+        
+        for (const tabRestaurant of tabRestaurants) {
+
+                $(`<div class="${tabRestaurant.name}"></div>`).appendTo('.restaurants');
+                $(`<h2>${tabRestaurant.name}</h2>`).appendTo('.' + tabRestaurant.name);
+                $(`<p>${tabRestaurant.address}</p>`).appendTo('.' + tabRestaurant.name);
+                $(`<p>${tabRestaurant.location.latitude}</p>`).appendTo('.' + tabRestaurant.name);
+                $(`<p>${tabRestaurant.location.longitude}</p>`).appendTo('.' + tabRestaurant.name);
+            }
 
     }
 
